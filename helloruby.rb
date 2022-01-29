c291a024bdf980822c9a9ee30991e077f00b5a74
Tetsuko11 <tetsuko1374@gmail.com>
2022-01-29 15:30:24 +0900

    initial commit

---
+++ helloruby.rb
@@ -0,0 +1,10 @@
+print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+# print ("Hello, Ruby.\n")
+#コメントアウト複数行 ctrl + /
+#gitリポジトリテスト
+#gitとGitHubの基本操作のテストです。
